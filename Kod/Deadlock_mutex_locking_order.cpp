54c7fdbb2bd4d5fba527a79cbf9c69e0dee4e08b
gregtokarz <116297792+gregtokarz@users.noreply.github.com>
2024-11-23 15:12:55 +0100

    Rozwiązanie Deadlock kolejność blokowania muteksów

---
+++ Kod/Deadlock_mutex_locking_order.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+mutex mutex1;
+mutex mutex2;
+
+void funkcja1() {
+    lock_guard<mutex> lock1(mutex1); // Zawsze najpierw blokujemy mutex1
+    this_thread::sleep_for(chrono::milliseconds(100));
+    lock_guard<mutex> lock2(mutex2); // Następnie blokujemy mutex2
+    cout << "Watek 1: uzyskal oba muteksy" << endl;
+}
+
+void funkcja2() {
+    lock_guard<mutex> lock1(mutex1); // Zawsze najpierw blokujemy mutex1
+    this_thread::sleep_for(chrono::milliseconds(100));
+    lock_guard<mutex> lock2(mutex2); // Następnie blokujemy mutex2
+    cout << "Watek 2: uzyskal oba muteksy" << endl;
+}
+
+int main() {
+    thread t1(funkcja1);
+    thread t2(funkcja2);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+
+
