342212f9159e27470bc81b08fb009c27329dc534
gregtokarz <116297792+gregtokarz@users.noreply.github.com>
2024-11-23 15:05:57 +0100

    Rozwiązanie Deadlock mutex

---
+++ Kod/Deadlock_mutex.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+mutex mutex1;
+mutex mutex2;
+
+void funkcja1() {
+    lock(mutex1, mutex2); // Blokujemy oba muteksy naraz
+    lock_guard<mutex> lock1(mutex1, adopt_lock); // Przejmujemy już zablokowane muteksy
+    lock_guard<mutex> lock2(mutex2, adopt_lock);
+    cout << "Watek 1: uzyskal oba muteksy" << endl;
+}
+
+void funkcja2() {
+    lock(mutex1, mutex2); // Blokujemy oba muteksy naraz
+    lock_guard<mutex> lock1(mutex1, adopt_lock); // Przejmujemy już zablokowane muteksy
+    lock_guard<mutex> lock2(mutex2, adopt_lock);
+    cout << "Watek 2: uzyskal oba muteksy" << endl;
+}
+
+int main() {
+    thread t1(funkcja1);
+    thread t2(funkcja2);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+
